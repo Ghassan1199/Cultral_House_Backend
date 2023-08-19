5c562a547b8b65c7acefba7c6103985e030d08e2
faleh <alnayef.fale7@gmail.com>
2023-08-19 22:43:45 +0300

    new blah

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -496,7 +496,7 @@ const getActions = async (req, res) => {
             time = date;
 
 
-           details = details.toString()
+           details = details.toJSON();
             const act = { action, time, details };
 
             actions.push(act);
