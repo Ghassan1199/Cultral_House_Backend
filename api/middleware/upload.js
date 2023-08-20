879c29288c268abb22e994fb5498cbbdbc80ec26
Ghassan <johndoe@example.com>
2023-08-21 01:26:04 +0300

    can create the driectory if it doesn`t exist

--- api/middleware/upload.js
+++ api/middleware/upload.js
@@ -1,13 +1,16 @@
 const multer = require('multer');
 const path = require('path');
+const util = require('util'), fs = require('fs');
 
 
 const upload = (des) => {
+    !fs.existsSync(`./images/${des}`) && fs.mkdirSync(`./images/${des}`, { recursive: true })
+
 
     const storage = multer.diskStorage(
         {
             destination: (_, _2, cb) => {
-                cb(null,  `./images/${des}`)
+                cb(null, `./images/${des}`)
             },
             filename: (_, file, cb) => {
                 cb(null, Date.now() + path.extname(file.originalname))
